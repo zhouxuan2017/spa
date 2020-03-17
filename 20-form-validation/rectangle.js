ffa61ca3283e444d785e62e2a098b047dd77b63b
zhouxuan <1207469303@qq.com>
2020-03-17 16:31:12 +0800

    d

--- 20-form-validation/rectangle.js
+++ 20-form-validation/rectangle.js
@@ -27,6 +27,94 @@ var r=new Rectangle(w,h);
   $perimeter.val(r.perimeter());
   $area.val(r.area());
 })
+  //字符校验
+  //1.event keypress
+  //2.event argument get key value   e.key当前按了哪个键  e.target.value 文本框里面的值
+  //3.非法键屏蔽掉  e.preventDefault()
+  //4.合法字符还要考虑出现的位置  列如： . e E  -
+$width.keypress(function(e){
+  if(/[abcdf-zABCDF-Z`~!@#$%^&*()=_+[\]{}|;:'",<>/?\\]/.test(e.key))
+   {
+      e.preventDefault();
+      return;
+   }
+
+//合法字符e
+//允许出现在非科学计数法的数字末尾
+//允许出现在非科学计数法的数字中间
+//不允许出现在非科学计数法的数字前面
+//不允许出现在空文本中
+//不允许出现在负号后面
+//不允许出现在科学计数法(e 和E)数字的末尾、前面、中间
+
+var pos=e.target.selectionStart,
+    con=e.target.value;
+  if(e.key==='e')
+  {
+    if(pos===0||con.indexOf('e')!==-1||con.indexOf('E')!==-1)
+{
+  e.preventDefault();
+  return; 
+}
+   if(pos===1&&con.substring(0,1)==='-')
+{
+  e.preventDefault();
+  return;
+}
+  }
+//合法字符E
+
+
+
+//合法字符.
+
+
+//合法字符-
+ });
+
+  
+$height.keypress(function(e){
+  
+  
+  if(/[abcdf-zABCDF-Z`~!@#$%^&*()=_+[\]{}|;:'",<>/?\\]/.test(e.key))
+   {
+      e.preventDefault();
+      return;
+   }
+
+//合法字符e
+//允许出现在非科学计数法的数字末尾
+//允许出现在非科学计数法的数字中间
+//不允许出现在非科学计数法的数字前面
+//不允许出现在空文本中
+//不允许出现在负号后面
+//不允许出现在科学计数法(e 和E)数字的末尾、前面、中间
+
+var pos=e.target.selectionStart,
+    con=e.target.value;
+  if(e.key==='e')
+  
+  
+  {
+    if(pos===0||con.indexOf('e')!==-1||con.indexOf('E')!==-1)
+{
+  e.preventDefault();
+  return; 
+}
+   if(pos===1&&con.substring(0,1)==='-')
+{
+  e.preventDefault();
+  return;
+}
+  }
+//合法字符E
+
+
+
+//合法字符.
+  
+});
+
 
 $width.focusout(function(){
   //if(!validate(width)) select this;
